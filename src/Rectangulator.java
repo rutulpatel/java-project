95a095204963c027f79fa1b4931dbfd6ac757c0f
Rutul Patel <rutulpatel@Rutuls-MacBook-Pro.local>
2018-04-12 18:36:46 -0500

    initial java commit

---
+++ src/Rectangulator.java
@@ -0,0 +1,14 @@
+public class Rectangulator {
+    public static void main(String[] args) {
+        int length = Integer.parseInt(args[0]);
+        int width = Integer.parseInt(args[1]);
+
+        Rectangle myRectangle = new Rectangle(length, width);
+
+        String output = String.format("Your Rectangle \n Length: %d\nWidth: %d\nArea: %d\nPermieter %d\n\n", 
+                                        myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPermieter());
+
+        System.out.println(output); 
+
+    }
+}
